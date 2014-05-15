1a6f4064dead5858096f5f7f7881a45b44dd11c9
TAGOMORI Satoshi <tagomoris@gmail.com>
2014-05-15 19:31:10 +0900

    v0.0.2: plugin depends on fluentd v0.10.42 or later

--- lib/fluent/plugin/buf_lightening.rb
+++ lib/fluent/plugin/buf_lightening.rb
@@ -33,51 +33,5 @@ module Fluent
       return false if @buffer_chunk_records_limit && chunk.record_counter >= @buffer_chunk_records_limit
       true
     end
-
-    # TODO: remove w/ fluentd v0.10.42 (or td-agent including fluentd v0.10.42)
-    def emit(key, data, chain) # copy&paste from BasicBuffer, and fix to add hook point
-      key = key.to_s
-
-      synchronize do
-        top = (@map[key] ||= new_chunk(key))
-
-        if storable?(top, data) # hook point (FIXED THIS LINE ONLY)
-          chain.next
-          top << data
-          return false
-        elsif @queue.size >= @buffer_queue_limit
-          raise BufferQueueLimitError, "queue size exceeds limit"
-        end
-
-        if data.bytesize > @buffer_chunk_limit
-          $log.warn "Size of the emitted data exceeds buffer_chunk_limit."
-          $log.warn "This may occur problems in the output plugins ``at this server.``"
-          $log.warn "To avoid problems, set a smaller number to the buffer_chunk_limit"
-          $log.warn "in the forward output ``at the log forwarding server.``"
-        end
-
-        nc = new_chunk(key)
-        ok = false
-
-        begin
-          nc << data
-          chain.next
-
-          flush_trigger = false
-          @queue.synchronize {
-            enqueue(top)
-            flush_trigger = @queue.empty?
-            @queue << top
-            @map[key] = nc
-          }
-
-          ok = true
-          return flush_trigger
-        ensure
-          nc.purge unless ok
-        end
-
-      end  # synchronize
-    end
   end
 end
